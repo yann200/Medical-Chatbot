80af2e9f777677dba971c0cf7aacf8c67d2464d7
TAGUEYANG NOUME YANN <noumeyann@gmail.com>
2025-12-11 14:52:31 +0100

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author = "Yann Noume",
+    author_email="noumeyann@gmail.com",
+    packages = find_packages(),
+    install_requires=[]
+)
