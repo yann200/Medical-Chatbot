8a8771909154ea4b721453e79910864958801f3d
TAGUEYANG NOUME YANN <noumeyann@gmail.com>
2025-12-16 14:32:05 +0100

    modif dockerfile

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,15 @@ WORKDIR /app
 
 COPY . /app
 
-RUN pip install -r requirements.txt
-
-RUN pip install -U sentence-transformers
-
+# Installer les packages avec versions fixes
+RUN pip install --upgrade pip
+RUN pip install langchain==0.3.26 \
+                langchain-community==0.3.26 \
+                huggingface-hub==0.36.0 \
+                flask==3.1.1 \
+                pypdf==5.6.1\
+                python-dotenv==1.1.0\
+                langchain-pinecone==0.2.8\
+                langchain-openai==0.3.24\
+                sentence-transformers==5.2.0 \
 CMD ["python3", "app.py"]
--- src/prompt.py
+++ src/prompt.py
@@ -2,3 +2,27 @@ system_prompt = ("""You are a helpful medical assistant. Use the following conte
 If you don't know the answer, just say that you don't know, don't try to make up an answer."""
                  "\n\n"
                  "{context}")
+
+
+# check_versions.py
+import sys
+import os
+import langchain
+import langchain_community
+import langchain_openai
+import langchain_pinecone
+import sentence_transformers
+import huggingface_hub
+import flask
+
+print("Python version:", sys.version)
+print("OS environment:", os.name)
+print("--- Package versions ---")
+print("langchain:", langchain.__version__)
+print("langchain_community:", langchain_community.__version__)
+#print("langchain_openai:", langchain_openai.__version__)
+#print("langchain_pinecone:", langchain_pinecone.__version__)
+print("sentence-transformers:", sentence_transformers.__version__)
+print("huggingface-hub:", huggingface_hub.__version__)
+print("flask:", flask.__version__)
+
