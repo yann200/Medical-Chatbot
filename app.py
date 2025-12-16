311630be1b4115636d724c9fe5cfc6379cd13d5d
TAGUEYANG NOUME YANN <noumeyann@gmail.com>
2025-12-16 12:53:17 +0100

    mod app

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ docsearch = PineconeVectorStore.from_existing_index(
 
 
 system_prompt = ("""You are a helpful medical assistant. Use the following context to answer the question at the end.
-If you don't know the answer, just say that you don't know, don't try to make up an answer."""
+If you don't know the answer, just say that you don't know, don't try to make up an answers."""
                  "\n\n"
                  "{context}")
 
